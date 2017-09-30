eb809e0fbcfa3a40e6c7708f91309da21a23b24e
kyler <kyler.figgins@gmail.com>
2017-09-30 15:53:38 -0600

    first commit of game

---
+++ main.js
@@ -0,0 +1,49 @@
+
+var value = 0;
+var workers = 0;
+function addOne(){
+    value++;
+    $("#money").text(value);
+}
+
+function addWorker(){
+    if(value < 50){
+        console.log("no");
+        return;
+    }
+    workers++;
+    value = value - 50;
+    $("#money").text(value);
+}
+
+function update(progress) {
+    // Update the state of the world for the elapsed time since last render
+    if(workers > 0){
+        value += workers;
+    }
+    console.log(value);
+  }
+  
+function draw() {
+    $("#money").text(value);
+  }
+
+var framesPerSecond = 1;
+
+function loop(timestamp) {
+    var progress = timestamp - lastRender
+  
+    update(progress)
+    draw()
+  
+    lastRender = timestamp
+    setTimeout(function() {
+        requestAnimationFrame(loop);
+ 
+        // animating/drawing code goes here
+ 
+ 
+    }, 1000 / framesPerSecond);
+  }
+var lastRender = 0
+window.requestAnimationFrame(loop)
